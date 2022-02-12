0167cfd091ab913a9f4e18be0db8f741567eb483
Christian Mondorf <christian@mondorf.dk>
2022-02-12 06:26:45 +0100

    Week 2 updated, all exercises complete but Metropolises doesn't cover Bielefeld question.

--- src/Christian_Training/Christian_Woche2/Loops/ChristmasTree.java
+++ src/Christian_Training/Christian_Woche2/Loops/ChristmasTree.java
@@ -1,5 +1,42 @@
 package Christian_Training.Christian_Woche2.Loops;
 
+import java.util.Scanner;
+
 public class ChristmasTree {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Wie hoch soll der Baum sein?");
+        int hoehe = scanner.nextInt();
+
+        for (int i = 0; i < hoehe; i++ ){
+            // linke Seite
+            String linkeSeite = "";
+            for (int j = 0; j < hoehe; j++){
+                if (j < hoehe - i){
+                    linkeSeite += " ";
+                } else {
+                    linkeSeite += "*";
+                }
+            }
+            // rechte Seite
+            String rechteSeite = "";
+            for (int k = 0; k <= i; k++){
+                rechteSeite += "*";
+            }
+            String zeile = linkeSeite + rechteSeite ;
+            System.out.println(zeile);
+        }
+        String baumStamm = "";
+        for (int s = 0; s <= hoehe; s++){
+            if (s < hoehe ){
+                baumStamm += " ";
+            } else {
+                baumStamm += "I";
+            }
+        }
+        System.out.println(baumStamm);
+    }
 
 }
+
--- src/Christian_Training/Christian_Woche2/Loops/Triangles.java
+++ src/Christian_Training/Christian_Woche2/Loops/Triangles.java
@@ -1,5 +1,23 @@
 package Christian_Training.Christian_Woche2.Loops;
 
-public class Triangles {
+import java.util.Scanner;
 
+public class Triangles {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wie groß sollen deine Dreiecke sein?");
+        int hoehe = scanner.nextInt();
+        System.out.println("Wieviele von diesen Dreiecken möchtest du zeichnen? ");
+        int zahl = scanner.nextInt();
+
+        for (int i = 0; i < zahl; i++ ) {
+            for (int j = 0; j <= hoehe; j++){
+                String zeile = "";
+                for (int x = 0; x < j; x++){
+                    zeile += "*";
+                }
+                System.out.println(zeile);
+            }
+        }
+    }
 }
