a8f378829dee28ffed7756d735631907f57f79fb
Philipp Bomers <mail@philipp-bomers.de>
2022-02-15 12:18:19 +0100

    accomplish some array tasks

--- src/Philipp_Training/Philipp_Woche3/Arrays/MinimumAndMaximum.java
+++ src/Philipp_Training/Philipp_Woche3/Arrays/MinimumAndMaximum.java
@@ -1,4 +1,19 @@
 package Philipp_Training.Philipp_Woche3.Arrays;
 
 public class MinimumAndMaximum {
+    public static void main(String[] args) {
+        int[] input = new int[]{1,4,5,7,20000,-511,100,-200,400};
+
+        int min = input[0];
+        int max = input[0];
+        for (int i : input) {
+            if (i<min) min = i;
+            if (i>max) max = i;
+        }
+
+        System.out.print(
+                "Minimum: " + min +
+                "\nMaximum: " + max
+        );
+    }
 }
--- src/Philipp_Training/Philipp_Woche3/Arrays/PassThrough.java
+++ src/Philipp_Training/Philipp_Woche3/Arrays/PassThrough.java
@@ -1,4 +1,11 @@
 package Philipp_Training.Philipp_Woche3.Arrays;
 
 public class PassThrough {
+    public static void main(String[] args) {
+        Integer[] numbers = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        for (int i : numbers) {
+            if (i != numbers[0]) System.out.print(", ");
+            System.out.print(i);
+        }
+    }
 }
