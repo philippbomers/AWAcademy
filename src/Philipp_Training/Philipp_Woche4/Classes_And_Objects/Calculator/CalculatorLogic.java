eaa633c1fbd187cdc266d375c3b80605755a2bb0
Philipp Bomers <mail@philipp-bomers.de>
2022-02-16 17:54:56 +0100

    commit calculator (with GUI)

---
+++ src/Philipp_Training/Philipp_Woche4/Classes_And_Objects/Calculator/CalculatorLogic.java
@@ -0,0 +1,30 @@
+package Philipp_Training.Philipp_Woche4.Classes_And_Objects.Calculator;
+
+public class CalculatorLogic {
+
+    private int ergebnis = 0;
+
+    public void reset() {
+        this.ergebnis = 0;
+    }
+
+    public void plus(int summand) {
+        this.ergebnis = this.ergebnis + summand;
+    }
+
+    public void minus(int minuend) {
+        this.ergebnis = this.ergebnis - minuend;
+    }
+
+    public void mal(int multiplier) {
+        this.ergebnis = this.ergebnis * multiplier;
+    }
+
+    public void geteilt(int divisor) {
+        this.ergebnis = this.ergebnis / divisor;
+    }
+
+    public String getErgebnis() {
+        return String.valueOf(this.ergebnis);
+    }
+}
