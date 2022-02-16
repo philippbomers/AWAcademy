410ad799eba0034d7184641ce7de5627ff292ea1
Philipp Bomers <mail@philipp-bomers.de>
2022-02-16 19:32:08 +0100

    Commit computer shop

---
+++ src/Philipp_Training/Philipp_Woche4/Classes_And_Objects/ComputerShop/ComputerShopMain.java
@@ -0,0 +1,10 @@
+package Philipp_Training.Philipp_Woche4.Classes_And_Objects.ComputerShop;
+
+public class ComputerShopMain {
+    public static void main(String[] args) {
+        Computer PhilippsPC = new Computer("Dell", 32, true);
+        PhilippsPC.setPurchasingPrice(670.65);
+        PhilippsPC.setSellingPrice(455.59);
+        System.out.println(PhilippsPC.getProfit());
+    }
+}
