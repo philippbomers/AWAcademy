161bfae14efb7baaf6e3578a03cd2efeb93bfb8b
Philipp Bomers <mail@philipp-bomers.de>
2022-02-25 20:29:53 +0100

    delete test file

--- src/Philipp_Training/Uebung/Test.java
+++
@@ -1,8 +0,0 @@
-package Philipp_Training.Uebung;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hallo Baum");
-        System.out.println("test");
-    }
-}
