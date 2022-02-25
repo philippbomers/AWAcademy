0040b9bf447dec74572d92c2ef1142507698e166
Anja Schiebel <schiebel.anja@web.de>
2022-02-25 15:20:17 +0100

    Neuen Text angelegt

--- src/Philipp_Training/Uebung/Test.java
+++ src/Philipp_Training/Uebung/Test.java
@@ -3,6 +3,5 @@ package Philipp_Training.Uebung;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hallo Baum");
-        System.out.println("test");
     }
 }
