78fc62392adc245ff2dc8d8c4c4825a24725b506
mkatran <mehmetkatran@gmail.com>
2022-02-28 15:26:24 +0100

    Verbesserung

--- src/Philipp_Training/Uebung/test.java
+++ src/Philipp_Training/Uebung/test.java
@@ -3,6 +3,6 @@ package Philipp_Training.Uebung;
 public class test {
     public static void main(String[] args) {
         System.out.println("Sag hallo");
-
+        System.out.println("Hallo zurück");
     }
 }
