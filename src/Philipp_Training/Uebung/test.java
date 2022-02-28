818385fc3bd511b74c244e67a8846e6ddad49a82
NadineHemmeter <nadine-hemmeter222@t-online.de>
2022-02-28 15:44:49 +0100

    Noch eine Veränderung

--- src/Philipp_Training/Uebung/test.java
+++ src/Philipp_Training/Uebung/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Sag hallo");
         System.out.println("Hallo zurück");
+        System.out.println("Hallo zurück2");
     }
 }
