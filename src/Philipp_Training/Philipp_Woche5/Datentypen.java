39427cd7860860af178a064c716184238297dbdb
Philipp Bomers <mail@philipp-bomers.de>
2022-03-02 12:03:33 +0100

    week 5 training day 2

---
+++ src/Philipp_Training/Philipp_Woche5/Datentypen.java
@@ -0,0 +1,19 @@
+package Philipp_Training.Philipp_Woche5;
+
+public class Datentypen {
+    byte monate = 12,
+         seitenImFlyer = 3,
+         anzahlMaustasten = 3,
+         kleinwagenPS = 68
+    ;
+
+    short einwohnerKleinstadt = 21_000,
+        sportwagenPS = 240;
+
+    int einwohnerDeutschland = 83_200_000;
+    long weltbevoelkerung = 7_964_493_699L;
+    float euroUsdollarKurs = 1.11F;
+    double pi = 3.141592653589793238462643383279502884197,
+        inzidenz = 12.32134121421212355121234213;
+    char geschlecht = 'm';
+}
