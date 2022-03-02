dfc4e56847cd8b07424cbf8454ea69b3fe5d1df0
Nadine Hemmeter <nadine-hemmeter222@t-online.de>
2022-03-02 17:38:45 +0100

    Hochladen Dateien Pre-Studies Training

---
+++ src/Nadine_Training/Nadine_Woche4Aufgaben/src/Neu.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Neu {
+    public static void main(String[] args) {
+
+        Klasse vier = new Klasse();
+        System.out.println(vier.getZahl());
+
+        Math.random();
+
+
+
+}
+}
