ef4cc5262ef0bdfa3e90e56b6c4a31d7f485b44e
Philipp Bomers <mail@philipp-bomers.de>
2022-03-03 11:16:25 +0100

    week 5 training day 3

---
+++ src/Philipp_Training/Philipp_Woche5/day3/AsciiII.java
@@ -0,0 +1,35 @@
+package Philipp_Training.Philipp_Woche5.day3;
+
+
+public class AsciiII {
+    public static void main(String[] args) {
+        asciiIITable();
+    }
+
+    public static void asciiIITable() {
+
+        String output;
+        for (int column = -1; column < 8; column++) {
+            output = (column<0) ? "" : String.valueOf(column);
+            System.out.printf("%3s", output);
+        }
+
+        int asciiVal = 0;
+        for (int row = 0; row < 16; row++) {
+            System.out.printf("%n%3s", row);
+            for (int column = 0; column < 8; column++) {
+                if(asciiVal<32){
+                    asciiVal++;
+                    String asciicharacter = ".";
+                    System.out.printf("%3s", asciicharacter);
+                }
+                if(asciiVal>31 && asciiVal<127){
+                    String asciicharacter = String.valueOf((char) (asciiVal++));
+                    System.out.printf("%3s", asciicharacter);
+                }
+            }
+        }
+
+
+    }
+}
--- src/Philipp_Training/Philipp_Woche5/day3/Zeichenketten.java
+++ src/Philipp_Training/Philipp_Woche5/day3/Zeichenketten.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Zeichenketten {
     public static void main(String[] args) {
 
-        char[] zeichenKette = {'J','a','v','a','-','K','u','r','s'};
-        char[] zahlenArray = {'5','6','1'};
+        char[] zeichenKette = {'J', 'a', 'v', 'a', '-', 'K', 'u', 'r', 's'};
+        char[] zahlenArray = {'5', '6', '1'};
 
         System.out.println("zeichenKette = " + Arrays.toString(backward(zeichenKette)));
         System.out.println("backward(zeichenKette) = " + backwardString(zeichenKette));
@@ -16,40 +16,40 @@ public class Zeichenketten {
 
     }
 
-    public static char[] backward(char[] zeichenKette){
-        char [] backward = new char[zeichenKette.length];
-        int x = zeichenKette.length-1;
-        for (char letter : zeichenKette){
-            backward[x--]=letter;
+    public static char[] backward(char[] zeichenKette) {
+        char[] backward = new char[zeichenKette.length];
+        int x = zeichenKette.length - 1;
+        for (char letter : zeichenKette) {
+            backward[x--] = letter;
         }
         return backward;
     }
 
-    public static String backwardString(char[] zeichenKette){
+    public static String backwardString(char[] zeichenKette) {
         StringBuilder backward = new StringBuilder();
-        for (char letter : zeichenKette){
+        for (char letter : zeichenKette) {
             backward.insert(0, letter);
         }
         return backward.toString();
     }
 
-    public static String everySecondCharString(char[] zeichenKette){
-        StringBuilder everySecondChar= new StringBuilder();
-        for (int i=0; i < zeichenKette.length; i+=2){
+    public static String everySecondCharString(char[] zeichenKette) {
+        StringBuilder everySecondChar = new StringBuilder();
+        for (int i = 0; i < zeichenKette.length; i += 2) {
             everySecondChar.append(zeichenKette[i]);
         }
         return everySecondChar.toString();
     }
 
-    public static String charArrayToString(char[] charArray){
+    public static String charArrayToString(char[] charArray) {
         StringBuilder result = new StringBuilder();
-        for (char letter : charArray){
+        for (char letter : charArray) {
             result.append(letter);
         }
         return result.toString();
     }
 
-    public static int[] charArrayToInt(char[] charArray){
+    public static int[] charArrayToInt(char[] charArray) {
         int[] result = new int[charArray.length];
         for (int i = 0; i < charArray.length; i++) {
             result[i] = charArray[i];
