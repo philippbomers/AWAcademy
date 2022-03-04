ca044f71d2c2ccb43d487def553e5aede022bc44
Philipp Bomers <mail@philipp-bomers.de>
2022-03-04 20:14:30 +0100

    some improvements

--- src/Philipp_Training/Other/Minesweeper/MinesweeperBoard.java
+++ src/Philipp_Training/Other/Minesweeper/MinesweeperBoard.java
@@ -4,54 +4,19 @@ public class MinesweeperBoard {
     final private int width;
     final private int height;
     final private int bombs;
-    private String[][] fields;
+    final private boolean[][] bombFields;
     private boolean[][] openFields;
-    private boolean[][] bombFields;
     private boolean[][] markedFields;
 
-    public MinesweeperBoard(int width, int height, int bombs) {
+    public MinesweeperBoard(int width, int bombs) {
         this.width = width;
-        this.height = height;
+        this.height = width;
         this.bombs = bombs;
-        this.fields = new String[width][height];
-        this.openFields = new boolean[width][height];
-        this.bombFields = new boolean[width][height];
-        this.markedFields = new boolean[width][height];
+        this.openFields = new boolean[width][width];
+        this.bombFields = new boolean[width][width];
+        this.markedFields = new boolean[width][width];
 
         this.setStartConfiguration();
-    }
-
-    private int getRandomNumber() {
-        return (int) (Math.random() * this.width);
-    }
-
-    private void setStartConfiguration() {
-        this.setOpenField(getRandomNumber(), getRandomNumber());
-        for (int i = 0; i < this.bombs; i++) {
-            int randomRow = getRandomNumber();
-            int randomColumn = getRandomNumber();
-            if (!this.getBombFields(randomRow, randomColumn)) {
-                this.setBombField(randomRow, randomColumn);
-            } else {
-                i--;
-            }
-        }
-    }
-
-    private void setBombField(int row, int col) {
-        this.bombFields[row][col] = !this.bombFields[row][col];
-    }
-
-    private boolean getBombFields(int row, int col) {
-        return this.bombFields[row][col];
-    }
-
-    private void setOpenField(int row, int col) {
-        this.openFields[row][col] = !this.openFields[row][col];
-    }
-
-    private boolean getOpenField(int row, int col) {
-        return this.openFields[row][col];
     }
 
     public void createBoard() {
@@ -69,14 +34,81 @@ public class MinesweeperBoard {
         }
     }
 
+    private void setStartConfiguration() {
+        int randomRow;
+        int randomColumn;
+        this.setOpenFields(getRandomNumber(), getRandomNumber());
+
+        for (int i = 0; i < this.bombs; i++) {
+            randomRow = getRandomNumber();
+            randomColumn = getRandomNumber();
+            if (!this.getBombFields(randomRow, randomColumn)
+                    && !this.getOpenFields(randomRow, randomColumn)) {
+                this.setBombFields(randomRow, randomColumn);
+            } else {
+                i--;
+            }
+        }
+    }
+
     private String getSign(int row, int col) {
         String result = "X";
-        if (getOpenField(row, col)) {
+        if (getOpenFields(row, col)) {
             result = "O";
             if (getBombFields(row, col)) {
                 result = "B";
             }
+        } else {
+            if (getMarkedFields(row, col)) {
+                result = "M";
+            }
         }
         return result;
+    }
+
+    public void openAllFields() {
+        for (int row = 0; row < this.height; row++) {
+            for (int col = 0; col < this.width; col++) {
+                this.openFields[row][col] = true;
+            }
+        }
+    }
+
+    private int getRandomNumber() {
+        return (int) (Math.random() * this.width);
+    }
+
+    private void setBombFields(int row, int col) {
+        this.bombFields[row][col] = !this.bombFields[row][col];
+    }
+
+    private boolean getBombFields(int row, int col) {
+        return this.bombFields[row][col];
+    }
+
+    public void setOpenFields(int row, int col) {
+        this.openFields[row][col] = !this.openFields[row][col];
+        if (this.getBombFields(row, col) == this.getOpenFields(row, col)) {
+            this.getLoosingDialogue();
+        }
+    }
+
+    public void getLoosingDialogue() {
+        System.out.println("\nDU\nHAST\nVerloren\n");
+        openAllFields();
+        this.createBoard();
+        System.exit(0);
+    }
+
+    private boolean getOpenFields(int row, int col) {
+        return this.openFields[row][col];
+    }
+
+    public boolean getMarkedFields(int row, int col) {
+        return this.markedFields[row][col];
+    }
+
+    public void setMarkedFields(int row, int col) {
+        this.markedFields[row][col] = !this.markedFields[row][col];
     }
 }
--- src/Philipp_Training/Other/Minesweeper/MinesweeperGame.java
+++ src/Philipp_Training/Other/Minesweeper/MinesweeperGame.java
@@ -1,8 +1,34 @@
 package Philipp_Training.Other.Minesweeper;
 
+import java.util.Scanner;
+
 public class MinesweeperGame {
     public static void main(String[] args) {
-        MinesweeperBoard minesweeperBoard = new MinesweeperBoard(10,10,12);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Willkommen beim Minesweeper-Game!");
+        System.out.println("Wie hoch und breit soll das Feld sein?");
+        int width = Integer.parseInt(scanner.nextLine());
+        System.out.println("Wie viele Bomben soll es geben?");
+        int bombs = Integer.parseInt(scanner.nextLine());
+
+
+        MinesweeperBoard minesweeperBoard = new MinesweeperBoard(width, bombs);
         minesweeperBoard.createBoard();
+
+        while (true) {
+            System.out.println("\nBitte gib die Reihe ein: ");
+            int row = Integer.parseInt(scanner.nextLine());
+
+            System.out.println("Bitte gib die Spalte ein: ");
+            int col = Integer.parseInt(scanner.nextLine());
+
+            minesweeperBoard.setOpenFields(row, col);
+            minesweeperBoard.createBoard();
+
+            System.out.println("\nMöchtest du das Programm beenden? [Ja] oder [Return]");
+            if (scanner.nextLine().equalsIgnoreCase("ja")) {
+                return;
+            }
+        }
     }
 }
