5ef0d44bf5f6ba13a50aa0ce0c54aa46cc7a8ed4
Philipp Bomers <mail@philipp-bomers.de>
2022-03-08 21:03:13 +0100

    optimize code

--- src/Philipp_Training/Philipp_Woche5/day3/FizzBuzz.java
+++ src/Philipp_Training/Philipp_Woche5/day3/FizzBuzz.java
@@ -2,7 +2,6 @@ package Philipp_Training.Philipp_Woche5.day3;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-
         for (int i = 1; i < 100; i++) {
             StringBuilder value = new StringBuilder();
             if (i % 3 == 0) value.append("Fizz");
