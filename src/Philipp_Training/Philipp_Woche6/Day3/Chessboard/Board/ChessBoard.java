722b2f43e6f196ea3f782c6ef5c6c1a58ff4c0b7
Philipp Bomers <mail@philipp-bomers.de>
2022-03-11 08:47:20 +0100

    made code more readable

--- src/Philipp_Training/Philipp_Woche6/Day3/Chessboard/Board/ChessBoard.java
+++ src/Philipp_Training/Philipp_Woche6/Day3/Chessboard/Board/ChessBoard.java
@@ -10,71 +10,34 @@ public class ChessBoard {
 
         this.field = new ChessField[8][8];
 
-        /**
-         * creates a new chess field
-         */
+//      creates a new chess field
         for (int y = 0; y < 8; y++) {
             for (int x = 0; x < 8; x++) {
+                this.field[x][y] = new ChessField(x, y);
                 switch (y) {
                     case 0:
                         switch (x) {
-                            case 0, 7 -> {
-                                this.field[x][y] = new ChessField(x, y);
-                                this.field[x][y].setChessPiece(new Rook(true, x, y));
-                            }
-                            case 1, 6 -> {
-                                this.field[x][y] = new ChessField(x, y);
-                                this.field[x][y].setChessPiece(new Knight(true, x, y));
-                            }
-                            case 2, 5 -> {
-                                this.field[x][y] = new ChessField(x, y);
-                                this.field[x][y].setChessPiece(new Bishop(true, x, y));
-                            }
-                            case 3 -> {
-                                this.field[x][y] = new ChessField(x, y);
-                                this.field[x][y].setChessPiece(new Queen(true, x, y));
-                            }
-                            case 4 -> {
-                                this.field[x][y] = new ChessField(x, y);
-                                this.field[x][y].setChessPiece(new King(true, x, y));
-                            }
+                            case 0, 7 -> this.field[x][y].setChessPiece(new Rook(true, x, y));
+                            case 1, 6 -> this.field[x][y].setChessPiece(new Knight(true, x, y));
+                            case 2, 5 -> this.field[x][y].setChessPiece(new Bishop(true, x, y));
+                            case 3 -> this.field[x][y].setChessPiece(new Queen(true, x, y));
+                            case 4 -> this.field[x][y].setChessPiece(new King(true, x, y));
                         }
                         break;
                     case 1:
-                        this.field[x][y] = new ChessField(x, y);
                         this.field[x][y].setChessPiece(new Pawn(true, x, y));
                         break;
                     case 6:
-                        this.field[x][y] = new ChessField(x, y);
                         this.field[x][y].setChessPiece(new Pawn(false, x, y));
                         break;
                     case 7:
                         switch (x) {
-                            case 0, 7 -> {
-                                this.field[x][y] = new ChessField(x, y);
-                                this.field[x][y].setChessPiece(new Rook(false, x, y));
-                            }
-                            case 1, 6 -> {
-                                this.field[x][y] = new ChessField(x, y);
-                                this.field[x][y].setChessPiece(new Knight(false, x, y));
-                            }
-                            case 2, 5 -> {
-                                this.field[x][y] = new ChessField(x, y);
-                                this.field[x][y].setChessPiece(new Bishop(false, x, y));
-                            }
-                            case 3 -> {
-                                this.field[x][y] = new ChessField(x, y);
-                                this.field[x][y].setChessPiece(new Queen(false, x, y));
-                            }
-                            case 4 -> {
-                                this.field[x][y] = new ChessField(x, y);
-                                this.field[x][y].setChessPiece(new King(false, x, y));
-                            }
+                            case 0, 7 -> this.field[x][y].setChessPiece(new Rook(false, x, y));
+                            case 1, 6 -> this.field[x][y].setChessPiece(new Knight(false, x, y));
+                            case 2, 5 -> this.field[x][y].setChessPiece(new Bishop(false, x, y));
+                            case 3 -> this.field[x][y].setChessPiece(new Queen(false, x, y));
+                            case 4 -> this.field[x][y].setChessPiece(new King(false, x, y));
                         }
-                        break;
-                    default:
-                        this.field[x][y] = new ChessField(x, y);
-                        break;
                 }
             }
         }
