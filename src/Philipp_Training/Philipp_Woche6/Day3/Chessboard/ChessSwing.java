d3b2ae6bfcf38939ec09becfbb0832bbbb623d3e
Philipp Bomers <mail@philipp-bomers.de>
2022-03-13 13:02:25 +0100

    further development

--- src/Philipp_Training/Philipp_Woche6/Day3/Chessboard/ChessSwing.java
+++ src/Philipp_Training/Philipp_Woche6/Day3/Chessboard/ChessSwing.java
@@ -11,13 +11,16 @@ public class ChessSwing extends ChessBoard {
 
     private final JFrame window = new JFrame("Chess");
     private final ChessBoard chessBoard = new ChessBoard();
-    private final int king = 0;
+    private final JButton[] buttons = new JButton[64];
+    private int selectedButtonNumber = -1;
+    private JLabel turn;
 
     public ChessSwing() {
 
         this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.window.setVisible(true);
 
+        this.window.add(this.generateMenuBar(), BorderLayout.NORTH);
         this.window.add(this.generateFieldButton(), BorderLayout.CENTER);
         this.window.setSize(800, 400);
     }
@@ -29,19 +32,38 @@ public class ChessSwing extends ChessBoard {
      */
     private JPanel generateFieldButton() {
         JPanel fields = new JPanel(new GridLayout(8, 8));
-        JButton button;
+        int buttonNumber = 0;
         for (int y = 0; y < 8; y++) {
             for (int x = 0; x < 8; x++) {
+                int finalButtonNumber = buttonNumber;
                 if (super.getField(x, y).getChessPiece() != null) {
-                    button = this.createFieldButton(x, y);
+                    this.buttons[buttonNumber] = this.createFieldButton(x, y);
                 } else {
-                    button = new JButton();
+                    this.buttons[buttonNumber] = new JButton();
                 }
-                button.setBackground(x % 2 == 0 ^ y % 2 == 0 ? Color.WHITE : Color.LIGHT_GRAY);
-                fields.add(button);
+                this.buttons[buttonNumber].setName(x + " " + y);
+                this.buttons[buttonNumber].addActionListener(e -> this.getButtonAction(finalButtonNumber));
+                this.buttons[buttonNumber].setBackground(x % 2 == 0 ^ y % 2 == 0 ? Color.WHITE : Color.LIGHT_GRAY);
+                fields.add(this.buttons[buttonNumber++]);
             }
         }
         return fields;
+    }
+
+    private JMenuBar generateMenuBar() {
+        JMenuBar jMenuBar = new JMenuBar();
+        JButton restartButton = new JButton("Neustart");
+        restartButton.addActionListener(e -> {
+            window.dispose();
+            new ChessSwing();
+        });
+        jMenuBar.add(restartButton);
+        jMenuBar.add(Box.createRigidArea(new Dimension(50, 0)));
+        this.turn = new JLabel(chessBoard.isWhiteTurn() ? "Weiß" : "Schwarz");
+        turn.setBounds(this.turn.getX(), this.turn.getY(), 200, 50);
+        jMenuBar.add(this.turn);
+        jMenuBar.add(new JLabel(" ist am Zug"));
+        return jMenuBar;
     }
 
     private JButton createFieldButton(int x, int y) {
@@ -57,10 +79,35 @@ public class ChessSwing extends ChessBoard {
         ImageIcon imageIcon = new ImageIcon(this.getPicutresPath() + filename);
         Image image = imageIcon.getImage();
         ImageIcon newImageIcon = new ImageIcon(image.getScaledInstance((int) (this.window.getWidth() * 0.3), (int) (this.window.getHeight() * 1.1), 50));
-        JButton button = new JButton(newImageIcon);
-        button.setHorizontalTextPosition(SwingConstants.CENTER);
-        button.setVerticalTextPosition(SwingConstants.CENTER);
-        return button;
+        return new JButton(newImageIcon);
+    }
+
+    private void getButtonAction(int newButtonNumber) {
+
+        if(buttons[newButtonNumber].getIcon() != null){
+            this.buttons[newButtonNumber].setBackground(Color.CYAN);
+        }
+
+        if (this.selectedButtonNumber != -1 && this.buttons[newButtonNumber] != null && buttons[this.selectedButtonNumber].getIcon() != null) {
+            String[] xyOld = this.buttons[this.selectedButtonNumber].getName().split(" ");
+            int oldX = Integer.parseInt(xyOld[0]);
+            int oldY = Integer.parseInt(xyOld[1]);
+
+            String[] xyNew = this.buttons[newButtonNumber].getName().split(" ");
+            int newX = Integer.parseInt(xyNew[0]);
+            int newY = Integer.parseInt(xyNew[1]);
+
+            this.buttons[selectedButtonNumber].setBackground(oldX % 2 == 0 ^ oldY % 2 == 0 ? Color.WHITE : Color.LIGHT_GRAY);
+            this.buttons[newButtonNumber].setBackground(newX % 2 == 0 ^ newY % 2 == 0 ? Color.WHITE : Color.LIGHT_GRAY);
+
+            if (chessBoard.movePieceOnBoard(oldX, oldY, newX, newY)) {
+                this.buttons[newButtonNumber].setIcon(buttons[selectedButtonNumber].getIcon());
+                this.buttons[selectedButtonNumber].setIcon(null);
+                this.turn.setText(chessBoard.isWhiteTurn() ? "Weiß" : "Schwarz");
+            }
+        }
+
+        this.selectedButtonNumber = Objects.equals(selectedButtonNumber, -1) ? newButtonNumber : -1;
     }
 
     private String getPicutresPath() {
